8ee933e3550f27ae5d4cfab871f9924b5554b5b4
GusGo <gus.gonzalez.15@gmail.com>
2020-01-16 19:50:30 -0500

    adding algolia

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 var something = 'does this work';
+
+var placesAutocomplete = places({
+	appId: 'plHJ3V77N7R0',
+	apiKey: '15c839632d2df1c8823fe9d3cd8988f5',
+	container: document.querySelector('#address-input')
+});
+
+console.log(placesAutocomplete);
